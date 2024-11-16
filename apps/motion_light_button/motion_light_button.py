3217c3906c3b6c5a15be9c554778a02d66bce8b5
root <root@core-ssh.local.hass.io>
2024-11-16 22:40:58 +0100

    Motion light with extra button added

--- apps/motion_light_button/motion_light_button.py
+++ apps/motion_light_button/motion_light_button.py
@@ -1,82 +1,75 @@
 import appdaemon.plugins.hass.hassapi as hassapi
-import time
+import time, importlib
+import shared.timeout
+importlib.reload( shared.timeout )
 
 # Args:
 # sensor: binary sensor to use as trigger
 # light : entity to turn on when detecting motion
 # delay: amount of time after turning on to turn off again. If not specified defaults to 60 seconds.
 #
-
 class MotionLightButtonActor:
-    def __init__( self, api, name, sensor, light, delay, button_delay, **kwargs ):
+    def __init__( self, api, name, sensor, light, motion_delay, button_delay, **kwargs ):
         self.api    = api
         self.name   = name
-        self.timer  = None
         self.light  = light
-        self.delay  = delay
-        self.button_delay   = button_delay
-        self.expiry = 0
-        self.light_state_set = None
+        self.motion_delay = motion_delay
+        self.button_delay = button_delay
+
+        self.timer = shared.timeout.DelayedCallback( api, self.light_off )
+        self.light_state_we_set = None # remember if it was us who set the light
         
-        api.listen_state( self.on_sensor, sensor )
-        api.listen_state( self.on_light, light )
+        api.listen_state( self.on_sensor, sensor )  # motion detector
+        api.listen_state( self.on_light, light )    # relay state change from wired button
         api.log( "initialize %s" % kwargs )
 
     def cancel(self):
-        self.cancel_timer()
+        self.timer.cancel()
     
-    def cancel_timer( self ):
-        api = self.api
-        if self.timer:
-            api.cancel_timer( self.timer )
-            self.timer = None
-    
-    def set_timer( self, delay ):
-        api = self.api
-        self.cancel_timer()
-        t = time.monotonic()
-        self.expiry = max( self.expiry, t+delay )
-        delay = self.expiry-t
-        api.log( "set_timer %s", delay )
-        self.timer = api.run_in( self.light_off, delay )
-
-    def light_off( self, kwargs ):
+    def light_off( self ):
         api = self.api
         api.log( "light_off" )
-        self.timer = None
-        self.light_state_set = "off"      # used in on_light()
+        self.light_state_we_set = "off"
         api.turn_off( self.light )
+
+    def light_on( self ):
+        api = self.api
+        api.log( "light_on" )
+        self.light_state_we_set = "on"
+        api.turn_on( self.light )
     
     # sensor state change
     def on_sensor( self, entity, attribute, old, new, kwargs ):
         api = self.api
         api.log( "on_sensor(%s -> %s)" % (old, new) )
-        if new == "on":
-            self.cancel_timer()           # no countdown when sensor detects
-            self.light_state_set = "on"   # used in on_light()
-            api.log( "light_on" )
-            api.turn_on( self.light )
-        elif api.get_state( self.light ) == "on":
-            self.set_timer( self.delay )  # begin countdown
+        if new == "on":                                 # presence detected
+            self.timer.cancel()                  # stop countdown when sensor detects motion
+            self.light_on()
+        elif api.get_state( self.light ) == "on":       # no presence detected and light is on
+            # Begin countdown. This will not shorten the timeout set by the button.
+            self.timer.at_least( self.motion_delay )
 
-    # relay state change
+    # relay state change from pushbutton
     def on_light( self, entity, attribute, old, new, kwargs ):
         api = self.api
-        if new != self.light_state_set:
+        if new != self.light_state_we_set:
             # triggered by button wired directly to relay (relay is retarded and does not report button state, only its own state)
-            self.light_state_set = None
+            self.light_state_we_set = None
             api.log( "on_light(%s -> %s)" % (old, new) )
             if new == "on":
-                # light turned on by button
-                self.set_timer( self.button_delay )
+                # light turned on by button: prolong timeout by button_delay
+                self.timer.at_least( self.button_delay )
             else:
                 # light turned off by button
-                self.expiry = 0
-                self.cancel_timer()
+                self.timer.reset()
+
 
 class MotionLightButton(hassapi.Hass):
     def initialize(self):
+        self.log( "args: %s", self.args )
         self.__actor = MotionLightButtonActor( self, **self.args )
 
     def cancel(self):
         self.__actor.cancel()
+
+
